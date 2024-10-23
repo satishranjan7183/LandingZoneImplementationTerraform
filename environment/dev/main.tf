6453dd278273b7fe07471c80dea20cdb8ec95b23
satish ranjan <satish.ranjan7183@gmail.com>
2024-10-24 00:02:39 +0530

    commit

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -1,5 +1,5 @@
 module "rgmodule" {
-  source = "../../module/load_balancer"
+  source = "../../module/VM_module"
   rgname = var.rgnamemodule
   rgloc =  var.rglocmodule
   enable_public_ip = var.enable_public_ip_module
