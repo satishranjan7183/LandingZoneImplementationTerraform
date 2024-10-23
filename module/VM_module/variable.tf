dcde5eafeb4cc1f981de37ee5dd8094fc1b7b8d2
satish ranjan <satish.ranjan7183@gmail.com>
2024-10-24 00:03:16 +0530

    added

---
+++ module/VM_module/variable.tf
@@ -0,0 +1,13 @@
+variable "rgname" {
+ type = string
+#   default = "devrg" 
+}
+variable "rgloc" {
+ type = string
+#   default = "West Europe" 
+}
+
+variable "enable_public_ip" {
+  type = bool
+#   default = false
+}
